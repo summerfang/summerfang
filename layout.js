309d8afdb2ea3e22f3d8f108481d79ed6a6983fe
Summer Fang <summerfang@gmail.com>
2023-03-17 23:42:24 -0700

    New version with bootstrap

---
+++ layout.js
@@ -0,0 +1,14 @@
+function header() { 
+    document.write('<div id="topmenu">');
+    document.write('<a href="develop.html">Develop</a>');
+    document.write('<a href="architecture.html">Architecture</a>');
+    document.write('<a href="about.html">About</a>')
+    document.write('<a href="https://www.webex.com" target="_blank">Webex</a>')    
+    document.write('</div>');
+}
+
+function footer() {
+    document.write('<div id="footer">');
+    document.write('<p> &copy; Summer Fang. All rights reserved.</p>');
+    document.write('</div>');
+}
--- showyears.js
+++
@@ -1,4 +0,0 @@
-var today = new Date();
-var year = today.getFullYear();
-var yearsPast2000 = year - 2000;
-document.getElementById("yearsPast2000").innerText = yearsPast2000;
