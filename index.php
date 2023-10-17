2964c25c547496f86a89b96a8b92a2c24f2b7af2
Summer Fang <summerfang@gmail.com>
2023-10-17 15:58:35 -0700

    sync code

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php include_once("index.html"); ?>
+<?php 
+include_once("index.html"); 
+
+$host = $_SERVER['HTTP_HOST'];
+
+if ($host == 'www.summerfang.me') {
+    header('Location: https://www.summerfang.me/index.html');
+    exit;
+} elseif ($host == 'www.weijiafang.com') {
+    header('Location: https://www.weijiafang.com/home.html');
+    exit;
+} else {
+    // Handle other cases or show an error message
+}
+?>
