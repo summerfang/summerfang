472ecc25378eca44aca4020d1a9ac8523831c21c
Summer Fang <summerfang@gmail.com>
2023-10-17 18:31:25 -0700

    Add routes to app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 @app.route('/hello')
 def hello():
